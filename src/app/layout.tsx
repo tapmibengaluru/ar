c7a3f7e84502a2c9627907adef668fb666b86e28
Jamshadj <developer@megamind.studio>
2026-05-18 19:31:30 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "TAPMI WebAR",
+  title: "",
   description: "Experience the future of campus interaction through our cutting-edge WebAR portal.",
 };
 
